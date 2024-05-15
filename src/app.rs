550808798b2444989057ca82c619dd22404ef424
fchamp <filipmhpersson@gmail.com>
2024-05-15 21:38:42 +0200

    soon ui working

--- src/app.rs
+++ src/app.rs
@@ -29,7 +29,31 @@ impl App {
             value_input: String::new(),
             pairs: HashMap::new(),
             current_screen: CurrentScreen::Main,
-            currently_editing: todo!(),
-        };
+            currently_editing: None,
+        }
+    }
+    pub fn save_key_value(&mut self) {
+        self.pairs
+            .insert(self.key_input.clone(), self.value_input.clone());
+        self.key_input = String::new();
+        self.value_input = String::new();
+        self.currently_editing = None;
+    }
+
+    pub fn toggle_editing(&mut self) {
+        if let Some(edit_mode) = &self.currently_editing {
+            match edit_mode {
+                CurrentlyEditing::Key => self.currently_editing = Some(CurrentlyEditing::Value),
+                CurrentlyEditing::Value => self.currently_editing = Some(CurrentlyEditing::Key),
+            }
+        } else {
+            self.currently_editing = Some(CurrentlyEditing::Key);
+        }
+    }
+
+    pub fn print_json(&self) -> serde_json::Result<()> {
+        let output = serde_json::to_string(&self.pairs)?;
+        println!("{}", output);
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
-use serde::Deserialize;
-use std::collections::HashMap;
+use crossterm::event::DisableMouseCapture;
+use crossterm::event::EnableMouseCapture;
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, LeaveAlternateScreen};
+use crossterm::terminal::{enable_raw_mode, EnterAlternateScreen};
+use ratatui::backend::CrosstermBackend;
+use ratatui::Terminal;
 use std::error::Error;
 use std::fs;
+use std::io;
 mod api;
 mod app;
 mod ui;
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+    enable_raw_mode()?;
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
     let file = fs::read_to_string("sample.json")?;
     let json: api::ApiDocs = serde_json::from_str(&file).unwrap();
-    dbg!("{}", json);
-    let app = app::App::new();
+    //dbg!("{}", json);
 
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+    let app = app::App::new();
+    let res = run_app(&mut terminal, &mut app);
+    dbg!("{}", app);
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Ok(do_print) = res {
+        if do_print {
+            app.print_json()?;
+        }
+    } else if let Err(err) = res {
+        println!("{err:?}")
+    }
     Ok(())
 }
